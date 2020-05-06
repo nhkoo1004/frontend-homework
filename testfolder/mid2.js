4b52d24c3c622f04eff7c03ebdd9426308fee9df
koo nh <rnskagh95@gamil.com>
2020-05-06 10:18:42 +0900

    testfolder update

--- testfolder/mid2.js
+++ testfolder/mid2.js
@@ -1,10 +1,7 @@
-let a =[];
+var a =[];
 for(let i=0; i<100; ++i){
     a[i] = Math.ceil(Math.random()*100);
 }
-a.sort(compareString);
+a.sort();
 console.log(a);
 
-function compareNumber(i,j){
-    return i-j;
-}
