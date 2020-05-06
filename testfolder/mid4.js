87ea185c5c2cf1d810d9a04f0fc3e044e80af640
koo nh <rnskagh95@gamil.com>
2020-05-06 11:21:20 +0900

    testfolder update

--- testfolder/mid4.js
+++ testfolder/mid4.js
@@ -2,9 +2,10 @@ var a =[];
 for(let i=0; i<100; ++i){
     a[i] = Math.ceil(Math.random()*100);
 }
-a1.reduce(function(ac,current,100,array){
-    current%2==0
+a.reduce(function(ac,current,index,a){
+    if(current%2==0){
     return ++ac;
+    }
 }
-,0)
-
+,0);
+console.log(ac);
---
+++ testfolder/mid5.js
@@ -0,0 +1,1 @@
+let a = [ "hello.html, "world.js", "readme.txt" ];
